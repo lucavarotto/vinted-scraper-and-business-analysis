abe4855d3d01edaf3fe76926cdf23ee70b13a15d
Luca <lc.varotto@gmail.com>
2026-05-12 17:52:45 +0200

    12 maggio 17:52

--- Analisi/Analisi.R
+++ Analisi/Analisi.R
@@ -6,7 +6,10 @@ dati1 <- readr::read_csv("Scraping/datasets/vinted_sneakers_20260511_18-15.csv")
 dati2 <- readr::read_csv("Scraping/datasets/vinted_sneakers_20260512_15-02.csv")
 dati <- rbind(dati1, dati2)
 
-save(dati, file = "Scraping/datasets/dati.csv")
+readr::write_csv(dati, "Scraping/datasets/dati.csv")
+
+
+
 
 brand <- readr::read_csv("Scraping/google_trends_brand_country.csv")
 View(brand)
