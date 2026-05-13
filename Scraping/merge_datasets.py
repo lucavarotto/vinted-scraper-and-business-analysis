70cf3c44886ba6b74614ea53d713b1da7344b670
Luca <lc.varotto@gmail.com>
2026-05-13 16:34:43 +0200

    third_dataset

--- Scraping/merge_datasets.py
+++ Scraping/merge_datasets.py
@@ -3,10 +3,11 @@ import pandas as pd
 # Caricamento dei dataset
 dati1 = pd.read_csv("Progetto/Scraping/datasets/vinted_sneakers_20260511_18-15.csv")
 dati2 = pd.read_csv("Progetto/Scraping/datasets/vinted_sneakers_20260512_15-02.csv")
+dati3 = pd.read_csv("Progetto/Scraping/datasets/vinted_sneakers_20260513_15-08.csv")
 
 # concatenazione
 # ignore_index=True serve a rifare la numerazione delle righe da 0 a N
-dati = pd.concat([dati1, dati2], ignore_index=True)
+dati = pd.concat([dati1, dati2, dati3], ignore_index=True)
 
 # Verifica le dimensioni
 print(dati.shape)
