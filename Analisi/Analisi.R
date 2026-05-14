b17cc8ce3c6f933b1ca61f688695758511fa95dc
Luca <lc.varotto@gmail.com>
2026-05-14 16:03:30 +0200

    new_dataset

--- Analisi/Analisi.R
+++ Analisi/Analisi.R
@@ -14,7 +14,21 @@ cat("\nrighe uniche:", length(unique(dati$URL)), length(unique(dati$Item_ID)))
 dati <- dati[!duplicated(dati$Item_ID),]
 dim(dati)
 
+dati[dati$Seller_Is_Pro==T,]$Seller_User
+
+sort(table(dati$Seller_User), decreasing = T) |> 
+  head(70)
+
 sum((table(dati$Seller_User) |> sort())>1)
+
+ids <- intersect(which(stringr::str_count(dati$Other_Items_Previewed_URLs, pattern=stringr::fixed("|")) > 30),
+          which(!is.na(dati$Seller_User)))
+
+dati$Seller_User[ids]
+
+
+
+
 
 id <- grep(pattern="min", dati$Upload_Date_Raw)
 dati$Upload_Date_Raw[id] <- "0 ore"
--- Scraping/merge_datasets.py
+++ Scraping/merge_datasets.py
@@ -1,14 +1,18 @@
 import pandas as pd
+from os import listdir
+
+print(listdir("Progetto/Scraping/datasets"))
 
 # Caricamento dei dataset
 dati1 = pd.read_csv("Progetto/Scraping/datasets/vinted_sneakers_20260511_18-15.csv")
 dati2 = pd.read_csv("Progetto/Scraping/datasets/vinted_sneakers_20260512_15-02.csv")
 dati3 = pd.read_csv("Progetto/Scraping/datasets/vinted_sneakers_20260513_15-08.csv")
 dati4 = pd.read_csv("Progetto/Scraping/datasets/vinted_Puma_Blipstream_20260514_12-22.csv")
+dati5 = pd.read_csv("Progetto/Scraping/datasets/dati_seller_pro.csv")
 
 # concatenazione
 # ignore_index=True serve a rifare la numerazione delle righe da 0 a N
-dati = pd.concat([dati1, dati2, dati3, dati4], ignore_index=True)
+dati = pd.concat([dati1, dati2, dati3, dati4, dati5], ignore_index=True)
 
 # Verifica le dimensioni
 print(dati.shape)
