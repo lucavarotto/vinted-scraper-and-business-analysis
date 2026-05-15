c64ea4448c0a3e63e144a50fca41831e068b5a78
Luca <lc.varotto@gmail.com>
2026-05-15 12:53:39 +0200

    new_dataset

--- Scraping/merge_datasets.py
+++ Scraping/merge_datasets.py
@@ -8,11 +8,12 @@ dati1 = pd.read_csv("Progetto/Scraping/datasets/vinted_sneakers_20260511_18-15.c
 dati2 = pd.read_csv("Progetto/Scraping/datasets/vinted_sneakers_20260512_15-02.csv")
 dati3 = pd.read_csv("Progetto/Scraping/datasets/vinted_sneakers_20260513_15-08.csv")
 dati4 = pd.read_csv("Progetto/Scraping/datasets/vinted_Puma_Blipstream_20260514_12-22.csv")
-dati5 = pd.read_csv("Progetto/Scraping/datasets/dati_seller_pro.csv")
+dati5 = pd.read_csv("Progetto/Scraping/datasets/dati_pro_sellers.csv")
+dati6 = pd.read_csv("Progetto/Scraping/datasets/vinted_non-branded_sneakers_20260514_16-52.csv")
 
 # concatenazione
 # ignore_index=True serve a rifare la numerazione delle righe da 0 a N
-dati = pd.concat([dati1, dati2, dati3, dati4, dati5], ignore_index=True)
+dati = pd.concat([dati1, dati2, dati3, dati4, dati5, dati6], ignore_index=True)
 
 # Verifica le dimensioni
 print(dati.shape)
